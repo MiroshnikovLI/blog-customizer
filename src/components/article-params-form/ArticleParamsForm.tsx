8068bb88e697563a42f18b689a08003f100bb097
Miroshnikov <MiroshnikovLI@yandex.ru>
2025-03-16 18:03:12 +0300

    initialization of practical work

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,14 +1,136 @@
+import styles from './ArticleParamsForm.module.scss';
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import {
+	Dispatch,
+	SetStateAction,
+	SyntheticEvent,
+	useRef,
+	useState,
+} from 'react';
+import { Select } from 'src/ui/select';
+import { Separator } from 'src/ui/separator';
+import { RadioGroup } from 'src/ui/radio-group';
+import {
+	backgroundColors,
+	contentWidthArr,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+	OptionType,
+} from 'src/constants/articleProps';
+import { Text } from 'src/ui/text';
+import { useOutsideClickClose } from 'src/ui/select/hooks/useOutsideClickClose';
 
-import styles from './ArticleParamsForm.module.scss';
+export type settingProps = {
+	settingForm: ISettingForm;
+};
 
-export const ArticleParamsForm = () => {
+export type ISettingForm = {
+	fontFamily: OptionType;
+	setFontFamily: Dispatch<SetStateAction<OptionType>>;
+	fontSize: OptionType;
+	setFontSize: Dispatch<SetStateAction<OptionType>>;
+	fontColor: OptionType;
+	setFontColor: Dispatch<SetStateAction<OptionType>>;
+	bgColor: OptionType;
+	setBgColor: Dispatch<SetStateAction<OptionType>>;
+	containerWidth: OptionType;
+	setContainerWidth: Dispatch<SetStateAction<OptionType>>;
+	submit: () => void;
+	clear: () => void;
+};
+
+export const ArticleParamsForm = ({ settingForm }: settingProps) => {
+	const [isOpen, setIsOpen] = useState<boolean>(false);
+	const [classForm, setClassForm] = useState<string>(styles.container);
+	const rootRef = useRef<HTMLDivElement>(null);
+
+	function handleCloseOnEscape(evt: KeyboardEvent) {
+		evt.code === 'Escape' && togglePanel();
+	}
+
+	useOutsideClickClose({
+		isOpen,
+		rootRef,
+		onClose: togglePanel,
+		onChange: setIsOpen,
+	});
+
+	function togglePanel() {
+		isOpen
+			? (setIsOpen(false),
+			  setClassForm(`${styles.container}`),
+			  document.addEventListener('keydown', handleCloseOnEscape))
+			: (setIsOpen(true),
+			  setClassForm(`${styles.container} ${styles.container_open}`),
+			  document.removeEventListener('keydown', handleCloseOnEscape));
+	}
+
+	function handleFormAction(e: SyntheticEvent) {
+		e.preventDefault();
+		e.type === 'submit' ? settingForm.submit() : settingForm.clear();
+	}
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton isOpen={isOpen} onClick={togglePanel} />
+			<aside className={classForm} ref={rootRef}>
+				<form
+					className={styles.form}
+					onSubmit={handleFormAction}
+					onReset={handleFormAction}>
+					<Text as='h1' size={31} weight={800} uppercase dynamicLite>
+						Задайте параметры
+					</Text>
+					<div>
+						<Text as='h2' size={12} weight={800} uppercase dynamicLite>
+							Шрифт
+						</Text>
+						<Select
+							selected={settingForm.fontFamily}
+							options={fontFamilyOptions}
+							onChange={settingForm.setFontFamily}
+						/>
+					</div>
+					<RadioGroup
+						name={settingForm.fontSize.className}
+						options={fontSizeOptions}
+						selected={settingForm.fontSize}
+						title={'Размер шрифта'}
+						onChange={settingForm.setFontSize}
+					/>
+					<div>
+						<Text as='h2' size={12} weight={800} uppercase dynamicLite>
+							Цвет шрифта
+						</Text>
+						<Select
+							selected={settingForm.fontColor}
+							options={fontColors}
+							onChange={settingForm.setFontColor}
+						/>
+					</div>
+					<Separator /> {/* Линия разделитель */}
+					<div>
+						<Text as='h2' size={12} weight={800} uppercase dynamicLite>
+							Цвет фона
+						</Text>
+						<Select
+							selected={settingForm.bgColor}
+							options={backgroundColors}
+							onChange={settingForm.setBgColor}
+						/>
+					</div>
+					<div>
+						<Text as='h2' size={12} weight={800} uppercase dynamicLite>
+							Ширина контента
+						</Text>
+						<Select
+							selected={settingForm.containerWidth}
+							options={contentWidthArr}
+							onChange={settingForm.setContainerWidth}
+						/>
+					</div>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,98 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
-import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
+import {
+	ArticleParamsForm,
+	ISettingForm,
+} from './components/article-params-form/ArticleParamsForm';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
+import { defaultArticleState, OptionType } from './constants/articleProps';
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	const [fontFamily, setFontFamily] = useState<OptionType>(
+		defaultArticleState.fontFamilyOption
+	);
+	const [fontSize, setFontSize] = useState<OptionType>(
+		defaultArticleState.fontSizeOption
+	);
+	const [fontColor, setFontColor] = useState<OptionType>(
+		defaultArticleState.fontColor
+	);
+	const [bgColor, setBgColor] = useState<OptionType>(
+		defaultArticleState.backgroundColor
+	);
+	const [containerWidth, setContainerWidth] = useState<OptionType>(
+		defaultArticleState.contentWidth
+	);
+
+	const [setPage, setSetPage] = useState({
+		fontFamily: defaultArticleState.fontFamilyOption,
+		fontSize: defaultArticleState.fontSizeOption,
+		fontColor: defaultArticleState.fontColor,
+		bgColor: defaultArticleState.backgroundColor,
+		containerWidth: defaultArticleState.contentWidth,
+	});
+
+	function submit() {
+		setSetPage({
+			fontFamily: fontFamily,
+			fontSize: fontSize,
+			fontColor: fontColor,
+			bgColor: bgColor,
+			containerWidth: containerWidth,
+		});
+	}
+
+	function clear() {
+		setSetPage({
+			fontFamily: defaultArticleState.fontFamilyOption,
+			fontSize: defaultArticleState.fontSizeOption,
+			fontColor: defaultArticleState.fontColor,
+			bgColor: defaultArticleState.backgroundColor,
+			containerWidth: defaultArticleState.contentWidth,
+		});
+		setFontFamily(defaultArticleState.fontFamilyOption);
+		setFontSize(defaultArticleState.fontSizeOption);
+		setFontColor(defaultArticleState.fontColor);
+		setBgColor(defaultArticleState.backgroundColor);
+		setContainerWidth(defaultArticleState.contentWidth);
+	}
+
+	const settingForm: ISettingForm = {
+		fontFamily,
+		setFontFamily,
+		fontSize,
+		setFontSize,
+		fontColor,
+		setFontColor,
+		bgColor,
+		setBgColor,
+		containerWidth,
+		setContainerWidth,
+		submit,
+		clear,
+	};
+
 	return (
 		<main
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': setPage.fontFamily.value,
+					'--font-size': setPage.fontSize.value,
+					'--font-color': setPage.fontColor.value,
+					'--container-width': setPage.containerWidth.value,
+					'--bg-color': setPage.bgColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm settingForm={settingForm} />
 			<Article />
 		</main>
 	);
